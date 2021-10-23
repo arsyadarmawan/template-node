66ec6a31c526dd334320b1ea09ea08dc42dec12a
arsyadarmawan <thareeqarsyad@gmail.com>
2021-10-23 10:24:38 +0700

    update config

--- app/config.js
+++ app/config.js
@@ -2,5 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    serviceName: process.env.SERVICE_NAME
+    serviceName: process.env.SERVICE_NAME,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPort: process.env_DB_PORT,
+    dbPass: process.env.DB_PASS,
+    dbName: process.env.DB_NAME,
 }
